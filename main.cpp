700962e277fb12fb0135ce09ac5fd94dedb21fb1
Texan24 <>
2021-05-25 04:26:49 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Modified version of original C program"
+            << " (by Kerighan & Ritchie)\n";
+
+  std::cout << "Hello World!";
+
+  return 0;
+}
